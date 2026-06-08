e7fbcf60e1866b4ea6f37deed9034ac2da4d0801
mohammedshadilkdy-code <mohammedshadilkdy@gmail.com>
2026-06-08 08:48:54 +0530

    Set cat delay to absolute zero

--- discord-akki-bot/index.js
+++ discord-akki-bot/index.js
@@ -102,8 +102,8 @@ client.on('messageCreate', async (message) => {
         let delay = 0;
         
         if (catsSeen <= 2) {
-          // First 2 cats: Extremely fast (90ms to 100ms)
-          delay = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
+          // First 2 cats: As fast as physically possible (0ms delay)
+          delay = 0;
         } else {
           // 60% chance: 2 to 8 seconds
           // 40% chance: 10 to 25 seconds
